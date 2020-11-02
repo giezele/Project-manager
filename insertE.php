d37b0308b4828511680e6379f203d422990a01f5
giezele <giedre.zel@gmail.com>
2020-11-02 18:51:06 +0200

    Prepared statement added

--- insertE.php
+++ insertE.php
@@ -1,19 +1,23 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 require_once "dbConn.php"; 
 
 if(isset($_POST['submit']))
 {		
     $fullname = $_POST['fullname'];
 
-    $insert = mysqli_query($conn,"INSERT INTO `employees`(`name`) VALUES ('$fullname')");
+    // $insert = mysqli_query($conn,"INSERT INTO `employees`(`name`) VALUES ('$fullname')"); //no prepared statement
 
-    if(!$insert)
-    {
-        echo mysqli_error($link);
-    }
-    else
-    {
-        echo "Records added successfully.";
+    // prepare and bind
+    $sql = "INSERT INTO `employees`(`name`) VALUES (?)";
+    if($query = $conn->prepare($sql)) { 
+        $query->bind_param('s', $fullname);
+        $query->execute();
+        
+    } else {
+        $error = $conn->errno . ' ' . $conn->error;
+        echo $error; 
     }
 }
 
